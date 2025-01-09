7dbe333925243c7bb579ce1ea2e12829a76538ba
wnsrl7250 <wnsrl7250@naver.com>
2025-01-09 14:10:18 +0900

    [구성] 직접 만든 유틸리티 함수 분리

--- public/scripts/react-demo.ts
+++ public/scripts/react-demo.ts
@@ -31,31 +31,3 @@ console.group("React 요소 노드");
 console.dir(heading);
 console.dir(changeButton);
 console.groupEnd();
-
-/* -------------------------------------------------------------------------- */
-/* Own CreateElement Function                                               */
-/* -------------------------------------------------------------------------- */
-
-function createElement(type, props = {}, ...children) {
-  return {
-    $$typeof: Symbol("own.element"),
-    key: null,
-    type,
-    props: {
-      ...props,
-      children,
-    },
-  };
-}
-
-const ownElement = createElement(
-  "div",
-  {
-    id: "own",
-  },
-  "hello"
-);
-
-console.group("Own 요소 노드");
-console.log(ownElement);
-console.groupEnd();
