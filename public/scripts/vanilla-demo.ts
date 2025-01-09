7135aabd18c03d25bb95960392cb1a9fb747b978
wnsrl7250 <wnsrl7250@naver.com>
2025-01-09 14:02:05 +0900

    [실습] React 엘리먼트(요소) - React 요소란? - 직접 만든는 추상 요소

--- public/scripts/vanilla-demo.ts
+++ public/scripts/vanilla-demo.ts
@@ -11,6 +11,10 @@ const greetingEntryList = Object.entries(Greeting); // [[key, value], ...]
 
 // JavaScript Markup and Event Binding & Mount DOM Element Node
 
+/* -------------------------------------------------------------------------- */
+/* Creating DOM Element Node                                                 */
+/* -------------------------------------------------------------------------- */
+
 // 제어 대상 생성 o / 참조 x
 // const heading = document.querySelector('h1');
 const heading = document.createElement("h1");
@@ -21,6 +25,11 @@ heading.textContent = greetingList.at(0) as string;
 const changeButton = document.createElement("button");
 changeButton.setAttribute("type", "button");
 changeButton.textContent = "인사말";
+
+console.group("DOM 요소 노드");
+console.dir(heading);
+console.dir(changeButton);
+console.groupEnd();
 
 // 버튼에 이벤트 바인딩
 changeButton?.addEventListener("click", handleChangeGreetMessage);
