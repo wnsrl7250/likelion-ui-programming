e6c2992751b64c42bafc9d15726a1448fc1d1c5d
wnsrl7250 <wnsrl7250@naver.com>
2025-01-09 16:01:43 +0900

    [정리] 주석 제거

--- public/scripts/react-demo-v2.ts
+++ public/scripts/react-demo-v2.ts
@@ -12,8 +12,15 @@ const changeButton = React.createElement(
   "인사말"
 );
 
+// Namespace.module
+// React.Fragment (function)
+
+// 의미 없는 <div> 대신에 <React.Fragment>를 사용
+// 의미 없는 구조는 실제 DOM에 렌더링 되지 않아요!!!
+// console.log(React.Fragment);
+
 const wrapper = React.createElement(
-  React.Fragment,
+  React.Fragment, // type: HTMLTagNameString(e.g: 'a', 'table', ...) / Buit-in Component
   null,
   heading,
   changeButton
