99de6368f0116e3a0d30d66f74ddf682fc1c023e
wnsrl7250 <wnsrl7250@naver.com>
2025-01-10 13:11:29 +0900

    [실습] App 컴포넌트

---
+++ public/components/app.ts
@@ -0,0 +1,9 @@
+import React from "../lib/react";
+
+function App() {
+  return React.createElement("div", {
+    className: "app",
+  });
+}
+
+export default App;
---
+++ public/scripts/main.ts
@@ -0,0 +1,7 @@
+import React from "../lib/react";
+import ReactDOM from "../lib/react-dom/client";
+import App from "../components/app";
+
+ReactDOM.createRoot(document.getElementById("react")).render(
+  React.createElement(App)
+);
