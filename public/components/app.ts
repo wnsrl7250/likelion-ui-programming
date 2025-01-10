7e584c655d19e4353be58812c6cd0b6deab1854a
wnsrl7250 <wnsrl7250@naver.com>
2025-01-10 14:04:36 +0900

    [실습] Calcurator 컴포넌트 - 컴포넌트 재사용

--- public/components/app.ts
+++ public/components/app.ts
@@ -1,8 +1,23 @@
 import React from "../lib/react";
+import Calculator from "./calculator";
+
+const { createElement: h } = React;
 
 function App() {
-  return React.createElement("div", {
+  return h("div", {
     className: "app",
+    children: [
+      // 1
+      h(Calculator),
+      // 2
+      h(Calculator, { operator: "-" }),
+      // 3
+      h(Calculator, { elements: [12, 2] }),
+      // 4
+      h(Calculator, { elements: [-12, 23], operator: "*" }),
+      // 5
+      h(Calculator, { elements: [, 6], operator: "/" }),
+    ],
   });
 }
 
