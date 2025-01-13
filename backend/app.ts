df8aa6f7f6bf17fa1815d1a4f2e9401381beb5d2
wnsrl7250 <wnsrl7250@naver.com>
2025-01-13 09:47:01 +0900

    [구성] frontend / backend

--- backend/app.ts
+++ backend/app.ts
@@ -2,16 +2,16 @@
 // TypeScript + Express.js를 활용해 간단한 API 서버 구성
 // --------------------------------------------------------------------------
 
-import 'dotenv/config';
-import { resolve } from 'node:path';
-import type { Express } from 'express';
-import express from 'express';
+import "dotenv/config";
+import { resolve } from "node:path";
+import type { Express } from "express";
+import express from "express";
 
-import usersRouter from './routes/users';
+import usersRouter from "./routes/users";
 
 /* CONFIG. ------------------------------------------------------------------ */
 
-const HOSTNAME = 'localhost';
+const HOSTNAME = "localhost";
 // const HOSTNAME = process.env.HOSTNAME ?? 'localhost';
 const PORT = Number(process.env.PORT) ?? 4000;
 const MESSAGE = `웹 서버 구동 : http://${HOSTNAME}:${PORT}`;
@@ -22,12 +22,12 @@ const app: Express = express();
 
 /* Middleware --------------------------------------------------------------- */
 
-app.use(express.static(resolve(__dirname, '../public')));
+app.use(express.static(resolve(__dirname, "../frontend")));
 app.use(express.json());
 
 /* Routing ------------------------------------------------------------------ */
 
-app.use('/api/users', usersRouter);
+app.use("/api/users", usersRouter);
 
 /* Listening ---------------------------------------------------------------- */
 
